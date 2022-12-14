75c5c729e0d5f0fd32351a08615f11963422b612
mpsalunggg <putrasatria893@gmail.com>
2022-12-15 01:58:40 +0800

    create redirects file for router

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://muhamadputrasatria-fe2585063.netlify.app/" // TODO: replace this
 const studentName = "Muhamad Putra Satria" // TODO: replace this
 const studentId = "FE2585063" // TODO: replace this
 
